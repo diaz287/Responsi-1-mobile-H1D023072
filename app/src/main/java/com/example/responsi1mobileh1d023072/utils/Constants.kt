14a39711e1abb7e1b4153272697e61c1cd0d7fbc
diaz287 <khafrizadiaz28@gmail.com>
2025-10-24 23:13:31 +0700

    add file project

---
+++ app/src/main/java/com/example/responsi1mobileh1d023072/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.responsi1mobileh1d023072.utils
+
+object Constants {
+    const val BASE_URL = "https://api.football-data.org/v4/teams/"
+    const val AUTH_TOKEN = "074a5e886c10417d804dfa7db1853c7d"
+}
